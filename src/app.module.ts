68489b2fd7b98041d9e863bb7912faad73ca1f75
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-12 00:15:19 -0500

    feature: add load env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
